4c7daf0d9c827dc21ebbde07f0cb8584290b91d3
Datise <donald.ballard@hotmail.com>
2016-09-19 18:38:41 -0700

    not sure if right approach, must consider

--- src/action/cart.js
+++ src/action/cart.js
@@ -1,7 +1,8 @@
-import {ADD_ITEM, REMOVE_ITEM, SET_QUANTITY, CLEAR_ITEMS} from './types';
+import {ADD_ITEM, REMOVE_ITEM, SET_QUANTITY, SET_QUANTITY_OR_REMOVE, CLEAR_ITEMS} from './types';
 import {createAction} from 'redux-actions';
 
 export const add = createAction(ADD_ITEM);
 export const remove = createAction(REMOVE_ITEM);
 export const setQuantity = createAction(SET_QUANTITY);
+export const setQuantityOrRemove = createAction(SET_QUANTITY_OR_REMOVE);
 export const clear = createAction(CLEAR_ITEMS);
--- src/reducer/cart.js
+++ src/reducer/cart.js
@@ -1,5 +1,5 @@
 import {handleActions} from 'redux-actions';
-import {CLEAR_ITEMS, ADD_ITEM, REMOVE_ITEM, SET_QUANTITY} from 'action/types';
+import {CLEAR_ITEMS, ADD_ITEM, REMOVE_ITEM, SET_QUANTITY, SET_QUANTITY_OR_REMOVE} from 'action/types';
 import map from 'lodash/fp/map';
 import filter from 'lodash/fp/filter'
 export default handleActions({
@@ -23,8 +23,17 @@ export default handleActions({
     ...state,
     items: map(({id, ...rest}) => (
       target === id ? {id, ...rest, quantity} : {id, ...rest}
-    ), state.items);
+    ), state.items)
   }),
+  // [SET_QUANTITY_OR_REMOVE]: (state, {payload: {id: target, quantity}}) => (
+  //   return (dispatch, getState) => {
+  //     if(quantity < 1){
+  //       store.dispatch({type: 'REMOVE_ITEM', id: id})
+  //     }else{
+  //       store.dispatch({type: 'SET_QUANTITY', getState(), payload})
+  //     }
+  //   }
+  // ),
 }, {
   items: [],
 });
