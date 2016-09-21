339ff3697d38a0951da409d6f874f5439fb00e23
Datise <donald.ballard@hotmail.com>
2016-09-20 21:01:39 -0700

    pictures somehow stretched after merge but imgur is down at the moment so I'll try to fix later

--- src/component/products.js
+++ src/component/products.js
@@ -3,7 +3,7 @@ import Product from './product';
 import * as products from '../data/items';
 import Heading from './heading';
 import styles from './styles.css';
-import map from 'lodash/fp/map'
+import map from 'lodash/fp/map';
 export default () => (
   <div>
     <Heading><img src={'img/products_icon.png'} className={styles.headingImage} />Products</Heading>
